6ca6c3ebb15beb44adbfbf07cc883f42a20e5d9d
Dmitry Nasonov <gvardopo4ta@gmail.com>
2020-01-05 01:08:23 +0300

    add /new for new object

--- py/flask_solsys_app.py
+++ py/flask_solsys_app.py
@@ -2,7 +2,7 @@
 WRT https://www.tutorialspoint.com/flask/flask_sqlalchemy.htm
 """
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 from sql_solsys_db import Sobject, Classes
@@ -18,6 +18,22 @@ db = SQLAlchemy(app)
 def show_all():
     query = db.session.query(Sobject).all()
     return render_template('table.html', objects=query)
+
+@app.route('/new', methods = ['GET', 'POST'])
+def new():
+   if request.method == 'POST':
+      if not request.form['name'] or not request.form['size'] or not request.form['discoverdate']:
+         flash('Пожалуйста, заполните форму!', 'error')
+      else:
+         sobject = Sobject(request.form['anumber'], request.form['name'], request.form['runame'], \
+            request.form['size'], request.form['mass'], request.form['discoverdate'], request.form['classes'])
+
+         db.session.add(sobject)
+         db.session.commit()
+         flash('Запись успешно добавлена')
+         return redirect(url_for('show_all'))
+   return render_template('add_obj.html')
+
 
 if __name__ == '__main__':
     db.create_all()
