76db5636aa58254a6059e4875c22e402b757ed59
Dmitry Nasonov <gvardopo4ta@gmail.com>
2020-01-11 14:46:26 +0300

    move js functions to script.js

---
+++ dev/script.js
@@ -0,0 +1,29 @@
+function calc() {
+  const a = parseFloat(document.querySelector('#val1').value);
+  const b = parseFloat(document.querySelector('#val2').value);
+  const act = document.querySelector('#operator').value;
+  let result;
+  if (act == 'add')
+    result = a + b;
+  else if (act == 'min')
+    result = a - b;
+  else if (act == 'mul')
+    result = a * b;
+  else if (act == 'div')
+    result = a / b;
+  document.querySelector('#result').innerHTML = result;
+  console.log('Числа:', a, b);
+  console.log('Результат:', result);
+}
+function showHidePar(mode) {
+  var elems = document.querySelectorAll("p");
+  for (let i = 0; i < elems.length; i++)
+    elems[i].style.display = mode;
+}
+function changeDirection(checkBox) {
+  var flexContainer = document.getElementById('flex-container');
+  if (checkBox.checked)
+    flexContainer.style.flexDirection = 'column';
+  else
+    flexContainer.style.flexDirection = 'row';
+}
