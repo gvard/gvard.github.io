c5eb0b6067f7218d33d6d38f1d2d9b46abb5c64f
Dmitry Nasonov <gvardopo4ta@gmail.com>
2020-01-27 17:22:59 +0300

    Add more movie names

--- py/oscar/get_films_info.py
+++ py/oscar/get_films_info.py
@@ -10,7 +10,12 @@ OSCAR_FILM_NAMES = ['Green Book','The Shape of Water', 'Moonlight', 'Spotlight',
     'The Departed', 'Crash', 'Million Dollar Baby', 'The Lord of the Rings: The Return of the King', 'Chicago',
     'A Beautiful Mind', 'Gladiator', 'American Beauty', 'Shakespeare in Love', 'Titanic', 'The English Patient',
     'Braveheart', 'Forrest Gump', "Schindler's List", 'Unforgiven', 'The Silence of the Lambs', 'Dances with Wolves',
-    'Driving Miss Daisy', 'Rain Man', 'The Last Emperor', 'Platoon']
+    'Driving Miss Daisy', 'Rain Man', 'The Last Emperor', 'Platoon', 'Out of Africa', 'Amadeus', 'Terms of Endearment',
+    'Gandhi', 'Chariots of Fire', 'Ordinary People', 'Kramer vs. Kramer', 'The Deer Hunter', 'Annie Hall', 'Rocky',
+    "One Flew Over the Cuckoo's Nest", 'The Godfather: Part II', 'The Sting', 'The Godfather', 'The French Connection',
+    'Patton', 'Midnight Cowboy', 'Oliver!', 'In the Heat of the Night', 'A Man for All Seasons', 'The Sound of Music',
+    'My Fair Lady', 'Tom Jones', 'Lawrence of Arabia', 'West Side Story', 'The Apartment', 'Ben-Hur', 'Gigi',
+    'The Bridge on the River Kwai', 'Around the World in 80 Days', 'Marty', 'On the Waterfront']
 
 oscar_list = []
 for movie_name in OSCAR_FILM_NAMES:
