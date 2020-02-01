3cadd65392a105635853fc3e16c56019ce3cc6fa
Dmitry Nasonov <gvardopo4ta@gmail.com>
2020-02-01 20:59:51 +0300

    Work on cleanup, rewrite oscr data

--- py/sql_solsys_db.py
+++ py/sql_solsys_db.py
@@ -4,7 +4,7 @@ WRT https://docs.sqlalchemy.org/en/13/orm/tutorial.html
 
 import datetime
 from sqlalchemy import create_engine, Table, Column, ForeignKey, \
-    Integer, BigInteger, Float, String, Boolean, Date 
+    Integer, BigInteger, Float, String, Boolean, Date
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
