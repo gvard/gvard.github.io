6cc8daff2d4ef7979d9b69101947abc7f171c2a3
Dmitry Nasonov <gvardopo4ta@gmail.com>
2020-02-08 13:16:37 +0300

    Add simple guestbook files

---
+++ php/postform.php
@@ -0,0 +1,9 @@
+<div class="normalText">
+<form action="addbook.php" method="post">
+Сообщение<br><br>
+<textarea name="message" cols="50" rows="7" class="textbox" value="Wow there"></textarea><br><br>
+Имя<br><br>
+<input type="text" name="name" size="48" class="textbox" value="alm"><br><br>
+<input type="submit" value=" Submit " class="textbox">
+</form>
+</div>
