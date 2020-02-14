3c664dc8c3099462d3c80a6306c9e6a3cff1339a
Dmitry Nasonov <gvardopo4ta@gmail.com>
2020-02-14 17:27:22 +0300

    Add Parasite movie to the list

--- py/oscar/get_films_list.py
+++ py/oscar/get_films_list.py
@@ -1,16 +1,16 @@
 """Get kinopoisk.ru list page, find info for each movie
 via BeautifulSoup, print data as dictionary.
-"""
-
 https://www.kinopoisk.ru/lists/editorial/best_by_rotten_tomatoes/
 https://www.kinopoisk.ru/lists/editorial/best_by_cinematography_institute/
+"""
+
 
 import urllib.request
 
 from bs4 import BeautifulSoup
 
 #URL = 'https://www.kinopoisk.ru/top/lists/17/perpage/100/' # Oscar
-# URL = 'https://www.kinopoisk.ru/top/lists/52/perpage/100/' # World profit
+#URL = 'https://www.kinopoisk.ru/top/lists/52/perpage/100/' # World profit
 URL = 'https://www.kinopoisk.ru/top/lists/23/perpage/100/' # USA inflation corrected
 html = urllib.request.urlopen(URL).read()
 bs = BeautifulSoup(html, 'html.parser')
