5d0b46c34fc9c9b15e6c6db2051c72e97d87a656
Dmitry Nasonov <gvardopo4ta@gmail.com>
2020-02-24 23:34:40 +0300

    update stats, add nanosats.eu stats

--- py/mk_cosm_stats.py
+++ py/mk_cosm_stats.py
@@ -7,11 +7,28 @@ HEAD = mk_head("Космонавтика: статистика", script="") + "<
 N2YO_URL = "https://www.n2yo.com/"
 SPACEFLIGHT_URL = "https://www.worldspaceflight.com/bios/stats.php"
 SPACEFLIGHT1_URL = "https://www.worldspaceflight.com/bios/stats1.php"
+CURRENTLY_IN_SPACE_URL = "https://www.worldspaceflight.com/bios/currentlyinspace.php"
+NANOSATS_URL = "https://www.nanosats.eu/"
 
 
 def get_n2yo(soup):
     return int(soup.findAll("span", {"style": "color:#d50000"})[0].text)
 
+def get_nanosats(soup):
+    date = soup.findAll('h3')[-1].b.text.split('of')[1].strip()
+    lis = soup.find("ul", {"class": "ultwo center-align"}).findAll('li')
+    return date, lis
+
+soup = get_soup(NANOSATS_URL)
+DATE, LIS = get_nanosats(soup)
+NANOSATS_HTML = f"""<h2>Наноспутники</h2>
+<p><a href="{NANOSATS_URL}">База данных наноспутников</a> &ndash; до 10 кг для нестандартных типов и до 27U (30&ndash;40 кг) для
+<a href="https://www.nanosats.eu/cubesat">кубсатов</a>. Последнее обновление: {DATE}.</p>
+<ul>
+"""
+for li in LIS:
+    NANOSATS_HTML += str(li) + '\n'
+NANOSATS_HTML += '</ul>\n'
 
 soup = get_soup(N2YO_URL)
 number_tracking_objects = get_n2yo(soup)
@@ -45,4 +62,4 @@ SPACEFLIGHT_HTML = f"""<h2>Пилотируемая космонавтика</h2
 """
 
 with open(os.path.join(os.pardir, 'cosm', 'stats.html'), 'w', encoding="utf8") as handle:
-    print(HEAD + SPACEFLIGHT_HTML + N2_STATS_HTML + TAIL, file=handle)
+    print(HEAD + SPACEFLIGHT_HTML + N2_STATS_HTML + NANOSATS_HTML + TAIL, file=handle)
