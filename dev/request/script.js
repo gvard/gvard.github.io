f1a6cec7ab91f6e1205aada5f0e763779608baf2
Dmitry Nasonov <gvardopo4ta@gmail.com>
2020-02-27 20:00:46 +0300

    Add link and API request

--- dev/request/script.js
+++ dev/request/script.js
@@ -1,6 +1,8 @@
 const RANDUSR_URL = 'https://randomuser.me/api/';
 const REQRES_URL = 'https://reqres.in/api/products/3';
 const SW_URL = 'https://swapi.co/api/people/1/';
+const TZ_URL = 'http://api.timezonedb.com/v2.1/get-time-zone?key=7QR5BE7L232Z&format=json&by=zone&zone=Europe/Moscow';
+
 
 function appendUsers(usersArray) {
   for (let i = 0; i < usersArray.results.length; i += 1) {
@@ -30,25 +32,24 @@ function loadReq(URL, goFunc) {
   console.log('request.readyState 2:', request.readyState);
   request.onload = function() {
     console.log("request.responseText", request.responseText);
+    if (request.status >= 200 && request.status < 400) {
+      let res = JSON.parse(request.response);
+      console.log('Parsed JSON response:', res);
+      goFunc(res);
+    } else {
+      console.error('Error!')
+    }
   };
   request.send();
-  request.onreadystatechange = function () {
-    console.log('request.readyState 3:', request.readyState);
-    if (request.readyState === 4) { // запрос завершился
-      if (request.status === 200) { // завершился успешно
-        let res = JSON.parse(request.response);
-        console.log('Response:', res);
-        goFunc(res);
-      } else {
-        console.error('Error!') // завершился неуспешно
-      }
-    }
-  }
 }
 function showRes(res) {
   let q = document.getElementById("sp");
   q.innerHTML = res.data.year;
   q.style.color = res.data.color;
+}
+function showTime(res) {
+  let q = document.getElementById("sp");
+  q.innerHTML = res.formatted;
 }
 function showLuke(res) {
   let q = document.getElementById("sp");
@@ -63,4 +64,7 @@ function loadReqRes() {
 }
 function loadSW() {
   loadReq(SW_URL, showLuke);
+}
+function loadTZ() {
+  loadReq(TZ_URL, showTime);
 }
