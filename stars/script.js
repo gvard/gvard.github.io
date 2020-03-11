365fbbbc172d9172aa81c02a9393b44f2831ddb0
Dmitry Nasonov <gvardopo4ta@gmail.com>
2020-03-11 15:27:45 +0300

    Work on new content, small fixes in code

--- stars/script.js
+++ stars/script.js
@@ -5,20 +5,27 @@ const OBJTYPES = {
 };
 function logger(mode, len) {
   let displayMode = '';
-  if (mode == 'none')
+  if (mode == 'none') {
     displayMode = 'Hide';
-  else if (mode.includes('block'))
+  }
+  else if (mode.includes('block')) {
     displayMode = 'Show';
-  console.log(`${displayMode} ${len} objects`);
+  }
+  console.log(displayMode, len, 'objects');
 }
-function showHideThis(mode, classNam) {
-  const elems = document.querySelectorAll(classNam);
-  for (let i = 0; i < elems.length; i++)
+function showHideThis(checkBox) {
+  const elems = document.querySelectorAll(checkBox.name);
+  let mode = 'none';
+  if (checkBox.checked) {
+    mode = 'block';
+  }
+  for (let i = 0; i < elems.length; i += 1) {
     elems[i].style.display = mode;
+  }
   logger(mode, elems.length);
 }
 function getDivs(classNam) {
-  let mainTab = document.getElementsByClassName('tab main')[0];
+  const mainTab = document.getElementById('tab');
   return mainTab.getElementsByClassName(classNam);
 }
 function getDate(txt) {
@@ -38,12 +45,10 @@ function findDate() {
       console.log(today, daymon);
     }
   }
-// document.write(datetime);
-
 }
 function showHideByDate(mode) {
   const elems = getDivs('date');
-  const yearForSort = parseInt(document.querySelector('#year').value);
+  const yearForSort = parseInt(document.getElementById('year').value);
   let j = 0;
   for (let i = 0; i < elems.length; i++) {
     if (elems[i].innerText != 'None' && getDate(elems[i].innerText) > yearForSort) {
@@ -54,7 +59,7 @@ function showHideByDate(mode) {
   logger(mode, j);
 }
 function getMass(txt) {
-  if (txt == 'None')
+  if (!txt)
     return 0;
   return txt.includes("±") ? Number(txt.substring(0, txt.indexOf("±")) + txt.substring(txt.indexOf("E"))) : Number(txt);
 }
@@ -80,25 +85,23 @@ function getNames() {
 function toSort(classNam) {
   let objToSort = getDivs('obj');
   let ArrToSort = Array.prototype.slice.call(objToSort, 0);
-  var sortFunction;
+  let sortFunction;
   if (classNam == 'mass')
     sortFunction = getMass;
-  else if (classNam == 'date')
-    sortFunction = getDate;
   else if (classNam == 'size')
     sortFunction = getSize;
-  ArrToSort.sort(function(a, b) {
+  ArrToSort.sort(function (a, b) {
     let aord = sortFunction(a.getElementsByClassName(classNam)[0].innerText);
     let bord = sortFunction(b.getElementsByClassName(classNam)[0].innerText);
     return (aord > bord) ? 1 : -1;
   });
-  var parent = document.getElementsByClassName('main')[0];
+  let parent = document.getElementById('tab');
   parent.innerHTML = "";
   for (let i = 0, l = ArrToSort.length; i < l; i += 1)
     parent.appendChild(ArrToSort[i]);
 }
 function hide() {
-  var messageBox = document.getElementById('messageBox');
+  let messageBox = document.getElementById('messageBox');
   messageBox.style.display = 'none';
 }
 function mkPar(before, txt, after) {
@@ -107,33 +110,36 @@ function mkPar(before, txt, after) {
 function mkContents(obj) {
   let name = obj.getElementsByClassName("name")[0].getElementsByTagName("a")[0].innerText;
   name = mkPar('<b>', name, '</b>');
+  let angular = obj.getElementsByClassName("angular")[0].innerText;
+  angular = mkPar('Угловые размеры: ', angular, '&nbsp;mas');
   let size = obj.getElementsByClassName("size")[0].innerText;
-  size = mkPar('Радиус: ', size, '&nbsp;км');
+  size = mkPar('Радиус: ', size, '&nbsp;R<sub>☉</sub>');
   let mass = obj.getElementsByClassName("mass")[0].innerText;
-  mass = mkPar('Масса: ', mass, '&nbsp;кг');
-  let date = obj.getElementsByClassName("date")[0].innerText;
-  date = mkPar('Дата открытия: ', date, '');
+  mass = mkPar('Масса: ', mass, '&nbsp;M<sub>☉</sub>');
+  let spClass = obj.getElementsByClassName("class")[0].innerText;
+  spClass = mkPar('Спектральный класс: ', spClass, '');
+  let temp = obj.getElementsByClassName("temp")[0].innerText;
+  temp = mkPar('Температура: ', temp, '&nbsp;K');
+
   let type = "";
   for (let objClassNam of obj.className.split(" "))
     if (objClassNam in OBJTYPES)
       type = OBJTYPES[objClassNam];
   type = mkPar('Тип: ', type, '');
-  let deltaV = obj.getElementsByClassName("delta-v");
-  if (deltaV.length) {
-    deltaV = deltaV[0].innerText;
-    deltaV = mkPar('Δv: ', deltaV, '&nbsp;км/с');
-  }
-  else {
-    deltaV = "";
-  }
-  return `${name}${type}${size}${mass}${date}${deltaV}`;
+  let desc = obj.getElementsByClassName("desc");
+  if (desc.length)
+    desc = mkPar('', desc[0].innerText, '');
+  else
+    desc = "";
+  return name + type + angular + size + mass + spClass + temp + desc;
 }
+
 function show(divImg) {
   const obj = divImg.parentElement;
-  var contents = document.getElementById('contents');
+  let contents = document.getElementById('contents');
   contents.innerHTML = `${divImg.innerHTML}${mkContents(obj)}`;
-  var messageBox = document.getElementById('messageBox');
-  var x, y;
+  let messageBox = document.getElementById('messageBox');
+  let x, y;
   if (window.event) {
     x = window.event.clientX + document.documentElement.scrollLeft +
         document.body.scrollLeft;
