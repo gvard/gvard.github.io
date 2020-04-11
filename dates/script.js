1a213bea85f52dc311df1d80248c6d186ca73e91
Dmitry Nasonov <gvardopo4ta@gmail.com>
2020-04-11 18:33:13 +0300

    dates/ - show it as slides or as feed, geit it by date or by tag

--- dates/script.js
+++ dates/script.js
@@ -30,47 +30,8 @@ function getToday() {
   return { daymon: `${day}.${month}`, month: month, year: currentdate.getFullYear() };
 }
 const setAgo = (elmAgo, yr) => { if (yr) elmAgo.innerText = `${yr} ${WordNumberCase(yr)} `;};
-function showByTag() {
-  const elems = document.getElementsByClassName('wrap');
-  const tagVal = document.getElementById("tag").value;
-  const today = getToday();
-  let j = 0;
-  for (let i = 0; i < elems.length; i += 1) {
-    let vis = 'none';
-    if (elems[i].classList.contains(tagVal)) {
-      const elmDate = elems[i].getElementsByClassName('date')[0];
-      const year = parseInt(elmDate.innerText.slice(6, 10), 10);
-      const elmAgo = elmDate.parentElement.getElementsByClassName('ago')[0];
-      setAgo(elmAgo, today.year - year);
-      vis = 'block';
-      j += 1;
-    }
-    elems[i].style.display = vis;
-  }
-  console.log("Select", j, "events");
-}
-function carousel() {
-  const elems = document.getElementsByClassName("date");
-  const tmout = document.getElementById("tmout");
-  const today = getToday();
+function getSlidesByDate() {
   let toShow = [];
-  for (let i = 0; i < elems.length; i += 1) {
-    const year = parseInt(elems[i].innerText.slice(6, 10), 10);
-    const elmAgo = elems[i].parentElement.getElementsByClassName('ago')[0];
-    setAgo(elmAgo, today.year - year);
-    toShow.push(elems[i]);
-    elems[i].parentElement.style.display = "none";
-  }
-  slideIndex += 1;
-  if (slideIndex > elems.length)
-    slideIndex = 1;
-  if (toShow.length != 0)
-    toShow[slideIndex - 1].parentElement.style.display = "block";
-  else
-    document.body.innerHTML = "<h1>Нет дат в текущем наборе событий</h1>";
-  setTimeout(carousel, tmout.value);
-}
-function findDates() {
   const elems = document.getElementsByClassName('date');
   const gyear = document.getElementById("year");
   const gdaymon = document.getElementById("daymon");
@@ -84,7 +45,6 @@ function findDates() {
     let daymon = elems[i].innerText.slice(0, 5);
     let mon = elems[i].innerText.slice(3, 5);
     let year = parseInt(elems[i].innerText.slice(6, 10), 10);
-    let vis = 'none';
     if ((gdaymon.value.indexOf(".") == -1 && mon == gdaymon.value) ||
       (gyear.value == year && gdaymon.value == daymon) ||
       (!gyear.value && gdaymon.value == daymon) ||
@@ -93,12 +53,68 @@ function findDates() {
       {
         const elmAgo = elems[i].parentElement.getElementsByClassName('ago')[0];
         setAgo(elmAgo, today.year - year);
-        vis = 'block';
         j += 1;
+        toShow.push(elems[i].parentElement);
       }
-    elems[i].parentElement.style.display = vis;
   }
   console.log("Select", j, "events");
+  return toShow;
+}
+function getSlidesByTag(tag) {
+  const today = getToday();
+  let toShow = [];
+  if (!tag) tag = 'wrap';
+  const elems = document.getElementsByClassName(tag);
+  for (let i = 0; i < elems.length; i += 1) {
+    const year = parseInt(elems[i].getElementsByClassName("date")[0].innerText.slice(6, 10), 10);
+    const elmAgo = elems[i].getElementsByClassName('ago')[0];
+    setAgo(elmAgo, today.year - year);
+    toShow.push(elems[i]);
+  }
+  return toShow;
+}
+const hideAll = () => {document.querySelectorAll('.wrap').forEach((el) => { el.style.display = 'none';});};
+const showAll = (elems) => { elems.forEach((el) => { el.style.display = 'block';});};
+function showByTag() {
+  hideAll();
+  const tag = document.getElementById("tag").value;
+  const elems = getSlidesByTag(tag);
+  showAll(elems);
+}
+function showByDate() {
+  hideAll();
+  const elems = getSlidesByDate();
+  console.log(elems.length);
+  showAll(elems);
+}
+function setSlideStyle() {
+  document.getElementsByClassName('right')[0].style.display = 'none';
+  document.getElementById('head').style.display = 'none';
+  document.getElementsByClassName('left')[0].style.width = '100%';
+}
+function runSlides(elems) {
+  let slideIndex = 0;
+  const tmout = document.getElementById("tmout").value;
+  elems[slideIndex].style.display = 'block';
+  setInterval(function() {
+    elems[slideIndex].style.display = 'none';
+    slideIndex = (slideIndex + 1) % elems.length;
+    elems[slideIndex].style.display = 'block';
+  }, tmout);
+}
+function slidesByDate() {
+  hideAll();
+  setSlideStyle();
+  const elems = getSlidesByDate();
+  runSlides(elems);
+}
+function slidesByTag(tag) {
+  hideAll();
+  setSlideStyle();
+  if (!tag)
+    tag = document.getElementById("tag").value;
+  const elems = getSlidesByTag(tag);
+  runSlides(elems);
 }
 function makeArray() {
   let arr = [];
@@ -118,12 +134,3 @@ function makeArray() {
   }
   console.log(arr);
 }
-var slideIndex = 0;
-document.addEventListener('DOMContentLoaded', function() {
-  document.getElementById('caro').addEventListener('click', function() {
-    document.getElementsByClassName('right')[0].style.display = 'none';
-    document.getElementById('head').style.display = 'none';
-    document.getElementsByClassName('left')[0].style.width = '100%';
-    carousel();
-  });
-});
