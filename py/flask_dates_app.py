900fe7442ffbbd93feaf561bdfad41ddc1bf0009
Dmitry Nasonov <gvardopo4ta@gmail.com>
2020-04-21 19:34:06 +0300

    Update dates flask app

--- py/flask_dates_app.py
+++ py/flask_dates_app.py
@@ -15,9 +15,9 @@ from sql_dates_db import Tag, Img, Event
 
 base_path = os.getcwd()
 path = os.path.abspath(os.path.join(base_path, os.pardir))
-static_path = os.path.join(path, 'dates')
+# dates_static_path = os.path.join(path, 'dates')
 
-app = Flask(__name__, static_folder=static_path, static_url_path='')
+app = Flask(__name__, static_folder=path, static_url_path='')
 SQLITE_DB_FILENAME = 'dates.sqlite'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + SQLITE_DB_FILENAME
 app.config['SECRET_KEY'] = "another random string;)"
