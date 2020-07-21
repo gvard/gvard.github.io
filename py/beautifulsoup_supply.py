dba19b5ed9a6b8f0a2868cff23df5270aaafecb5
Dmitry Nasonov <gvardopo4ta@gmail.com>
2020-07-21 20:43:54 +0300

    update stats

--- py/beautifulsoup_supply.py
+++ py/beautifulsoup_supply.py
@@ -9,7 +9,7 @@ HEAD_HEAD = """<!DOCTYPE html>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <meta name="description" content="Подборка статистических данных в области астрономии, астрофизике и космонавтике. Файл сгенерирован автоматически.">
+  <meta name="description" content="Подборка статистических данных в области астрономии, астрофизики и космонавтики. Файл сгенерирован автоматически.">
 """
 HEAD_TAIL = """</head>
 """
