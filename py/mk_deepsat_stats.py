e29b1d7fdc1240221d0e46b24921a865f32959a4
Dmitry Nasonov <gvardopo4ta@gmail.com>
2021-06-08 22:42:35 +0300

    add video, clean deepcat infographic

--- py/mk_deepsat_stats.py
+++ py/mk_deepsat_stats.py
@@ -37,18 +37,18 @@ BODY = f"""<body">
   <ul>
     <li>Не покинули сферу Хилла Земли: на орбите <span class="yellow">{DATA[0][0]}</span>, вошли в атмосферу <span class="yellow">{DATA[0][1]}</span>, потеряны <span class="yellow">{DATA[0][2]}</span></li>
     <li>Вернулись к Земле с лунной или солнечной орбиты: на орбите <span class="yellow">{DATA[1][0]}</span>, вошли в атмосферу <span class="yellow">{DATA[1][1]}</span>, потеряны <span class="yellow">{DATA[1][2]}</span></li>
-    <li>Луна: на орбите <span class="yellow">{DATA[2][0]}</span>, достигли поверхночти <span class="yellow">{DATA[2][1]}</span>, потеряны <span class="yellow">{DATA[2][2]}</span></li>
-    <li>В точках Лаграгжа L1/L2 системы Земля-Солнце: на орбите <span class="yellow">{DATA[3][0]}</span>, вошли в атмосферу <span class="yellow">{DATA[3][1]}</span>, потеряны <span class="yellow">{DATA[3][2]}</span></li>
-    <li>Солнце: на орбите <span class="yellow">{DATA[4][0]}</span>, вошли в атмосферу <span class="yellow">{DATA[4][1]}</span>, потеряны <span class="yellow">{DATA[4][2]}</span></li>
-    <li>Меркурий: на орбите <span class="yellow">{DATA[5][0]}</span>, достигли поверхночти <span class="yellow">{DATA[5][1]}</span>, потеряны <span class="yellow">{DATA[5][2]}</span></li>
-    <li>Венера: на орбите <span class="yellow">{DATA[6][0]}</span>, вошли в атмосферу <span class="yellow">{DATA[6][1]}</span>, потеряны <span class="yellow">{DATA[6][2]}</span></li>
-    <li>Марс: на орбите <span class="yellow">{DATA[7][0]}</span>, достигли поверхночти <span class="yellow">{DATA[7][1]}</span>, потеряны <span class="yellow">{DATA[7][2]}</span></li>
-    <li>Юпитер: на орбите <span class="yellow">{DATA[8][0]}</span>, вошли в атмосферу <span class="yellow">{DATA[8][1]}</span>, потеряны <span class="yellow">{DATA[8][2]}</span></li>
-    <li>Сатурн: на орбите <span class="yellow">{DATA[9][0]}</span>, вошли в атмосферу <span class="yellow">{DATA[9][1]}</span>, потеряны <span class="yellow">{DATA[9][2]}</span></li>
-    <li>Титан: на орбите <span class="yellow">{DATA[10][0]}</span>, вошли в атмосферу <span class="yellow">{DATA[10][1]}</span>, потеряны <span class="yellow">{DATA[10][2]}</span></li>
-    <li>Астероиды и кометы: на орбите <span class="yellow">{DATA[11][0]}</span>, достигли поверхночти <span class="yellow">{DATA[11][1]}</span>, потеряны <span class="yellow">{DATA[11][2]}</span></li>
+    <li>Луна: на орбите <span class="yellow">{DATA[2][0]}</span>, достигли поверхночти <span class="yellow">{DATA[2][1]}</span></li>
+    <li>В точках Лаграгжа L1/L2 системы Земля-Солнце: на орбите <span class="yellow">{DATA[3][0]}</span>, вошли в атмосферу <span class="yellow">{DATA[3][1]}</span></li>
+    <li>Солнце: на орбите <span class="yellow">{DATA[4][0]}</span>, вошли в атмосферу <span class="yellow">{DATA[4][1]}</span></li>
+    <li>Меркурий: на орбите <span class="yellow">{DATA[5][0]}</span>, достигли поверхночти <span class="yellow">{DATA[5][1]}</span></li>
+    <li>Венера: на орбите <span class="yellow">{DATA[6][0]}</span>, вошли в атмосферу <span class="yellow">{DATA[6][1]}</span></li>
+    <li>Марс: на орбите <span class="yellow">{DATA[7][0]}</span>, достигли поверхночти <span class="yellow">{DATA[7][1]}</span></li>
+    <li>Юпитер: на орбите <span class="yellow">{DATA[8][0]}</span>, вошли в атмосферу <span class="yellow">{DATA[8][1]}</span></li>
+    <li>Сатурн: на орбите <span class="yellow">{DATA[9][0]}</span>, вошли в атмосферу <span class="yellow">{DATA[9][1]}</span></li>
+    <li>Титан: на орбите <span class="yellow">{DATA[10][0]}</span>, вошли в атмосферу <span class="yellow">{DATA[10][1]}</span></li>
+    <li>Астероиды и кометы: на орбите <span class="yellow">{DATA[11][0]}</span>, достигли поверхночти <span class="yellow">{DATA[11][1]}</span></li>
   </ul>
-  <p>Всего объектов: <span class="yellow">{ALL}</span></p>
+  <p>Всего записей: <span class="yellow">{ALL}</span></p>
   </div>
   <div id="footer">
     <h2>Центр Астрономического и&nbsp;космического образования</h2>
