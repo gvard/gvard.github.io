ad6989e60c9dc2f40657a4ebb1a756fa946ea7ea
Dmitry Nasonov <gvardopo4ta@gmail.com>
2021-09-29 15:01:31 +0300

    update stats, fix typo

--- py/mk_stats.cmd
+++ py/mk_stats.cmd
@@ -3,5 +3,5 @@ python mk_deepsat_stats.py
 python mk_grb_stats.py
 python mk_launches_stats.py
 python mk_solsys_stats.py
-python mk_spacesebris_stats.py
+python mk_spacedebris_stats.py
 python mk_star_stats.py
