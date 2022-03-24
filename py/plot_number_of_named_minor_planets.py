7974d801c0e8bba8d25e7030fb6ac4d04328d2ac
Dmitry Nasonov <gvardopo4ta@gmail.com>
2022-03-24 19:40:39 +0300

    add python script for plotting number of named minor planets

---
+++ py/plot_number_of_named_minor_planets.py
@@ -0,0 +1,37 @@
+"""Python script for reading specific parameter in all versions of
+html page from git repo.
+How to get all previous version of a specific file/folder:
+https://stackoverflow.com/questions/12850030/git-getting-all-previous-version-of-a-specific-file-folder
+"""
+
+import re, os
+from datetime import datetime
+
+from bs4 import BeautifulSoup
+import matplotlib.pyplot as plt
+
+tmp_path = os.path.join(os.pardir, 'tmp', 'all_versions_exported')
+filenames = next(os.walk(tmp_path), (None, None, []))[2]
+print(filenames)
+dats, nums = [], []
+date_pattern = re.compile(
+    "\d+.(\d{1,2})-(Jan?|Feb?|Mar?|Apr?|May|Jun?|"
+    "Jul?|Aug?|Sep?|Oct?|Nov?|"
+    "Dec?)-(\d{4}).*")
+
+for fname in filenames:
+    with open(os.path.join(tmp_path, fname)) as html:
+        (day, mon, yr) = date_pattern.findall(fname)[0]
+        dats.append(datetime.strptime(day + "." + mon + "." + yr,
+            "%d.%b.%Y").date())
+        soup = BeautifulSoup(html, 'html.parser')
+        names_txt = soup.findAll('li')[-1].text
+        num = int(names_txt[13:15] + names_txt[16:19])
+        nums.append(num)
+
+plt.plot(dats, nums, 'o-')
+plt.xlabel('Время', fontsize=14)
+plt.ylabel('Количество малых планет с именами', fontsize=14)
+ 
+plt.grid()
+plt.show()
