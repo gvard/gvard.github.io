1f13b1aaa475b211f92034db5add5210d0e49d5d
Dmitry Nasonov <gvardopo4ta@gmail.com>
2022-10-11 01:28:07 +0300

    update script for plotting number of manned flights

--- py/manned/mk_allflights_graph.py
+++ py/manned/mk_allflights_graph.py
@@ -1,6 +1,11 @@
+"""Python script for plotting graph
+with manned flight statistics.
+"""
+
 import urllib.request
 import datetime
-import os, ssl
+import os
+import ssl
 
 from bs4 import BeautifulSoup
 import matplotlib.pyplot as plt
@@ -14,8 +19,9 @@ if (not os.environ.get('PYTHONHTTPSVERIFY', '') and
 
 
 def get_table(url):
+    """Read ASCII data table from pre html element"""
     html = urllib.request.urlopen(url).read()
-    #html = open(url)
+    #html = open(url) # for local file
     soup = BeautifulSoup(html, 'html.parser')
     return soup.findAll('pre')[1].text.splitlines()
 
@@ -24,14 +30,17 @@ DATA_URL = "https://planet4589.org/space/astro/lists/"
 DATA_URL += "missions.html"
 
 data = get_table(DATA_URL)[1:]
-dates, nums = [], []
-astro_num = 0
+dates, flights_nums, rides_nums = [], [], []
+FLIGHTS_NUM = 0
+RIDES_NUM = 0
 for line in data:
     if line and line[1] == '0':
         dd = datetime.datetime.strptime(line[77:93], '%Y %b %d %H%M')
         dates.append(dd)
-        astro_num += int(line[139])
-        nums.append(astro_num)
+        FLIGHTS_NUM += 1
+        RIDES_NUM += int(line[139])
+        flights_nums.append(FLIGHTS_NUM)
+        rides_nums.append(RIDES_NUM)
 
 fig, ax = plt.subplots(figsize=(16, 9))
 fig.subplots_adjust(0.048, 0.06, 0.99, 0.97)
@@ -41,16 +50,25 @@ ax.xaxis.set_major_locator(locator)
 ax.xaxis.set_minor_locator(AutoMinorLocator(5))
 
 tdlt = datetime.timedelta(days=630)
+
+nums, ylim_top_margin = rides_nums, 50
+#nums, ylim_top_margin = flights_nums, 15
+
 accidents = [(1967, 1, 27), (1967, 4, 23), (1971, 6, 29), (1986, 1, 28), (2003, 2, 1)]
+private_spaceflights = [(2020, 5, 30), (2021, 7, 11), (2021, 7, 20), (2021, 9, 16)]
 for ac_date in accidents:
     ac_d = datetime.date(*ac_date)
-    plt.plot([ac_d, ac_d], [0, 1400], '--r')
+    plt.plot([ac_d, ac_d], [0, nums[-1]+ylim_top_margin], '--r')
+for ac_date in private_spaceflights:
+    ac_d = datetime.date(*ac_date)
+    plt.plot([ac_d, ac_d], [0, nums[-1]+ylim_top_margin], '--g')
 
 plt.plot(dates, nums, '.b', ms=6)
 plt.xlim(dates[0]-tdlt, dates[-1]+tdlt)
-plt.ylim(0, nums[-1]+50)
-plt.title('Рост числа полетов астронавтов. Всего ' + str(len(dates)) + ' полета и ' + str(nums[-1]) + ' человек к ноябрю 2021 года')
+plt.ylim(0, nums[-1]+ylim_top_margin)
+plt.title('Рост числа полетов астронавтов. Всего ' + str(len(dates)) + \
+    ' полета и ' + str(rides_nums[-1]) + ' человек к октябрю 2022 года')
 plt.xlabel('Время, годы', fontsize=14)
 plt.ylabel('Количество полетов астронавтов', fontsize=14)
 plt.grid()
-plt.savefig('graph-mannedflights.png', dpi=120)
+plt.savefig('graph-mannedflights-rides.png', dpi=120)
