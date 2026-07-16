6295618a95afe7a9e387b0ad9f1fe3762821bdb9
Dmitry Nasonov <1260248+gvard@users.noreply.github.com>
2026-07-16 19:58:55 +0300

    update solarsystem/resolved/

---
+++ solarsystem/resolved/script.js
@@ -0,0 +1,197 @@
+"use strict";
+document.addEventListener('DOMContentLoaded', () => {
+const grid = document.getElementById('tab');
+grid.addEventListener('mouseover', (event) => {
+  const imgDiv = event.target.closest('.obj .img');
+  if (imgDiv) {
+    const baseImg = imgDiv.querySelector('.img img');
+    let finalImgUrl;
+    const imgData = imgDiv.dataset.b;
+    if (!imgData) {
+      finalImgUrl = baseImg.getAttribute('src');
+    }
+    else if (imgData.startsWith('http')) {
+      finalImgUrl = imgData;
+    }
+    else {
+      finalImgUrl = `../images/${imgData}`;
+    }
+  show(event, imgDiv, finalImgUrl);
+  }
+});
+grid.addEventListener('mouseout', (event) => {
+  const imgDiv = event.target.closest('.obj .img');
+  const relatedTarget = event.relatedTarget ? event.relatedTarget.closest('.obj .img') : null;
+  if (imgDiv && imgDiv !== relatedTarget) {
+    hide();
+  }
+});
+});
+const OBJTYPES = {
+  star: "звезда", pla: "планета", neo: "околоземный объект", pha: "потенциально опасный астероид", co: "ядро кометы", ea: "спутник Земли", mar: "спутник Марса", mab: "объект основного пояса астероидов", innermb: "объект внутренней части пояса астероидов", dw: "карликовая планета", dwmoon: "спутник карликовой планеты", ju: "спутник Юпитера", sat: "спутник Сатурна", ur: "спутник Урана", ne: "спутник Нептуна", pl: "спутник Плутона", tno: "транснептуновый объект", centaur: "кентавр", contactbinary: "контактно-двойная", sca: "объект рассеянного диска", satellite: "спутник малой планеты", jfc: "комета семейства Юпитера"
+};
+function ObjParams() {
+  const objs = getDivs('obj');
+  let objArr = [];
+  let objDct = [];
+  let datesObj = {};
+  for (let i = 0; i < objs.length; i += 1) {
+    const name = objs[i].getElementsByClassName('name')[0].innerText;
+    const classes = objs[i].className.replace('obj ', '').split(' ');
+    const a = objs[i].getElementsByClassName('name')[0].getElementsByTagName("a")[0];
+    const objRuName = a.text;
+    const img = objs[i].getElementsByClassName('img')[0].getElementsByTagName("img")[0];
+    const imgPath = img.src.split('/');
+    const size = getSize(objs[i].getElementsByClassName('size')[0].innerText);
+    const mass = txtMass(objs[i].getElementsByClassName('mass')[0]?.innerText ?? "");
+    const date = objs[i].getElementsByClassName('date')[0]?.innerText ?? "";
+    const deltaV = getDeltaV(objs[i].getElementsByClassName('deltav')[0].innerText);
+    const type = mkType(objs[i].className, OBJTYPES);
+    let otkrWord = "Открыт ";
+    if (type.includes("ланета") || type.includes("комет"))
+      otkrWord = "Открыта "
+    const slug = otkrWord + type + ' ' + objRuName;
+    let desc = "";
+    if (objs[i].getElementsByClassName('desc')[0])
+      desc = objs[i].getElementsByClassName('desc')[0].innerText;
+    if (!datesObj[date.slice(0, -5)])
+      datesObj[date.slice(0, -5)] = [];
+    datesObj[date.slice(0, -5)].push({ 'year': date.slice(6, 10), 'slug': slug, 'img': 'solarsystem/images/' + imgPath[imgPath.length - 1], 'desc': desc });
+    objDct.push({ 'name': img.alt, 'runame': objRuName, 'size': size, 'mass': mass, 'date': date, 'classes': classes });
+    objArr.push([img.alt, objRuName, size, mass, date, deltaV, classes, imgPath[imgPath.length - 1]]);
+  }
+  console.log(datesObj);
+  console.log(objArr);
+  console.log(objDct);
+}
+function getDate(txt) {
+  if (!txt) {
+    return 0;
+  }
+  return Number(txt.slice(6, 10));
+}
+function showHideByDate(mode) {
+  const elems = getDivs('date');
+  const yearForSort = parseInt(document.getElementById('year').value, 10);
+  let j = 0;
+  for (let i = 0; i < elems.length; i += 1) {
+    if (elems[i].innerText && getDate(elems[i].innerText) > yearForSort) {
+      elems[i].parentElement.style.display = mode;
+      j += 1;
+    }
+  }
+  logger(mode, j);
+}
+function getDeltaV(txt) {
+  return txt.includes(">") ? parseFloat(txt.substring(txt.indexOf(">") + 1, txt.length)) : parseFloat(txt);
+}
+function toSort(classNam) {
+  let sortFunction;
+  if (classNam == 'mass')
+    sortFunction = getMass;
+  else if (classNam == 'dens')
+    sortFunction = getSize;
+  else if (classNam == 'size')
+    sortFunction = getSize;
+  else if (classNam == 'date')
+    sortFunction = getDate;
+  else if (classNam == 'imgdat')
+    sortFunction = getDate;
+  else if (classNam == 'deltav')
+    sortFunction = getDeltaV;
+  doSort(sortFunction, classNam);
+}
+function show(evnt, divImg, imgPth) {
+  const obj = divImg.parentElement;
+  let name = divImg.getElementsByTagName('img')[0].alt;
+  let des = obj.getElementsByClassName("des");
+  if (des.length) {
+    des = mkPar('<b>Обозначение</b>: ', des[0].innerText, '');
+  } else {
+    des = "";
+  }
+  let size = obj.getElementsByClassName("size")[0].innerText;
+  size = mkPar('<b>Радиус</b>: ', size, '&nbsp;км');
+  let imdis = obj.dataset.id;
+  imdis = mkPar('<b>Изображение получено с расстояния</b> ', imdis, '&nbsp;км');
+  try {
+    var mass = obj.getElementsByClassName("mass")[0].innerText.trim();
+  } catch (error) {
+    if (error instanceof TypeError) {
+      var mass = "";
+    }
+  }
+  if (mass)
+    mass = mkPar('<b>Масса</b>: ', mass, '&nbsp;кг');
+  let dens = obj.getElementsByClassName("dens");
+  if (dens.length) {
+    dens = mkPar('<b>Плотность</b>: ', dens[0].innerText, '&nbsp;г/см<sup>3</sup>');
+  } else {
+    dens = "";
+  }
+  try {
+    var date = obj.getElementsByClassName("date")[0].innerText;
+  } catch (error) {
+    if (error instanceof TypeError) {
+      var date = "";
+    }
+  }
+  if (date)
+    date = mkPar('<b>Дата открытия</b>: ', date, '');
+  let imda = obj.getElementsByClassName("imgdat");
+  if (imda.length) {
+    imda = mkPar('<b>Первые подробные изображения</b>: ', imda[0].innerText, '');
+  } else {
+    imda = "";
+  }
+  let pre = obj.getElementsByClassName("pre");
+  if (pre.length) {
+    pre = mkPar('<b>Первые наблюдения</b>: ', pre[0].innerText, '');
+  } else {
+    pre = "";
+  }
+  let deltaV = obj.getElementsByClassName("deltav");
+  if (deltaV.length) {
+    deltaV = mkPar('<b>Δv</b>: ', deltaV[0].innerText, '&nbsp;км/с');
+  } else {
+    deltaV = "";
+  }
+  let rot = obj.dataset.r;
+  rot = mkPar('<b>Период вращения</b>: ', rot, '&nbsp;суток');
+  let type = mkType(obj.className, OBJTYPES);
+  type = mkPar('<b>Типы</b>: ', type, '');
+  let desc = obj.getElementsByClassName("desc");
+  if (desc.length)
+    desc = mkPar('', desc[0].innerText, '');
+  else
+    desc = "";
+  const contentToShow = `<img src="${imgPth}"><h2>${name}</h2>` + des + type + size + mass + dens + rot + deltaV + date + imda + pre + imdis + desc;
+  toShow(evnt, contentToShow);
+}
+function getToday() {
+  const currentdate = new Date();
+  const day = ("0" + currentdate.getDate()).slice(-2);
+  const month = ("0" + (currentdate.getMonth() + 1)).slice(-2);
+  return { daymon: `${day}.${month}`, month: month, year: currentdate.getFullYear() };
+}
+function findDate() {
+  const elems = document.getElementsByClassName('date');
+  const today = getToday();
+  let objOfDay = [];
+  let objOfMonthDiv = document.getElementById('objOfMonth');
+  let objOfDayDiv = document.getElementById('objOfDay');
+  for (let i = 0; i < elems.length; i += 1) {
+    let daymon = elems[i].innerText.slice(0, 5);
+    const mon = elems[i].innerText.slice(3, 5);
+    const divName = elems[i].parentElement.getElementsByClassName('name')[0];
+    const objHtml = `${divName.innerHTML}. Дата открытия: ${elems[i].innerHTML}<br>`;
+    if (mon == today.month)
+      objOfMonthDiv.innerHTML += objHtml;
+    if (today.daymon == daymon) {
+      objOfDayDiv.innerHTML += objHtml;
+      objOfDay.push(objHtml);
+    }
+  }
+  if (!objOfDay.length)
+    objOfDayDiv.style.display = 'none';
+}
